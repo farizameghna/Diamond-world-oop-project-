9ab3c23da7488b90804d26f54dd096a4ba7e934f
farizameghna <almazifariza445@gmail.com>
2025-04-05 09:41:38 -0400

    add

--- Dimond_World_Oop_Project_Group14/src/main/java/Promotion.java
+++ Dimond_World_Oop_Project_Group14/src/main/java/Promotion.java
@@ -1,2 +1,46 @@
+import java.util.Date;
+
 public class Promotion {
+    Integer id;
+    String name;
+    Date datetime;
+
+    public Promotion(Integer id, String name, Date datetime) {
+        this.id = id;
+        this.name = name;
+        this.datetime = datetime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", datetime=" + datetime +
+                '}';
+    }
 }
